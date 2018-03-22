b1d7ed70ee7179aa0a86f9d1dae3f66201e6054e
Terexit <teresamarbancastro@gmail.com>
2018-03-22 12:29:21 +0100

    input feelings

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -8,13 +8,17 @@ class Editor extends Component {
         <div className="date__container">
           <h4 className="date__title">Fecha</h4>
           <input className="date__input" placeholder="22/03/2018">
-     {/* <span className="date__icon"><img src={calendarPic} alt="calendario"/></span> */}
-          </input>
-        </div>
-        <div className="mood__container">
-          <h4 className="mood__title">Estado de ánimo</h4>
-          <input className="mood__select" type="checkbox"></input>
-          <input className="mood__select" type="checkbox"></input>
+          {/* <span className="date__icon"><img src={calendarPic} alt="calendario"/></span> */}
+        </input>
+      </div>
+      <div className="mood__container">
+        <h4 className="mood__title">Estado de ánimo</h4>
+        {/* <input className="mood__select" type="radio"></input> */}
+        <label htmlFor="feeling1">:)</label>
+        <input id="feeling1" type="radio" value="happy" name="feelings"/>
+        <label htmlFor="feeling2" value="sad" name="feelings">:(</label>
+          <input id="feeling2" type="radio"/>
+          {/* <input className="mood__select" type="radio"></input> */}
 
         </div>
       </div>
